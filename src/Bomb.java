4ca538c8d64f85f47c224f5fca04d5bda60dfaa7
BT_1N3_30 <sample@bths.edu>
2024-03-15 09:00:26 -0400

    weapons decided, and subclass deleted

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,2 +1,21 @@
-public class Bomb {
+public class Bomb extends Weapon {
+    private int inventory;
+    public Bomb(int spaces, String name){
+        super(spaces, name);
+        inventory = 3;
+    }
+
+    public int getInventory(){
+        return inventory;
+    }
+
+    public void setInventory(int num){
+        inventory = num;
+    }
+
+    @Override
+    public void printSound(){
+        System.out.println("BOOM!");
+    }
+
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,20 @@
 public class Weapon {
+    private int spaces;
+    private String name;
+
+    public Weapon(int spaces, String name){
+        this.spaces = spaces;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getSpaces(){
+        return spaces;
+    }
+
+    public void printSound(){
+        System.out.println("Pew! Pew!");
+    }
 }
