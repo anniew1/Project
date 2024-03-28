0f2ca92a625aec8706446980ce5263fff80f9b62
BT_1N3_29 <dwa@>
2024-03-28 09:29:01 -0400

    updated adding ship methods

--- src/Player.java
+++ src/Player.java
@@ -13,25 +13,49 @@ public class Player {
         placeShip();
     }
 
-    //places the ships on the board
+    // prints out the board with the ships
+    public void printShipBoard() {
+        for (Water[] row : shipBoard) {
+            for (Water col : row) {
+                System.out.print(col.getSymbol());
+            }
+            System.out.println();
+        }
+    }
+
+    // places the ships
+    private void addShips(int shipRow, int shipCol, String direction, int length) {
+        for (int i = 0; i < length; i++) {
+            if (direction.equals("vertical")) {
+                shipBoard[shipRow + i][shipCol] = new Ship("0");
+            } else {
+                shipBoard[shipRow][shipCol + i] = new Ship("0");
+            }
+        }
+    }
+
+
+    //asks the player to place ships
     private void placeShip() {
         System.out.println("Ship # = how much spaces (Ship 1 = 1 space long, Ship 2 = 2 spaces long, etc.)");
         for (int i = 0; i < 5; i++) {
-            System.out.print("Where would you like to place ship " + (i + 1) + " (row col): ");
-            String position = scan.nextLine();
+            System.out.print("Where would you like to place ship " + (i + 1) + " (row): ");
+            int shipRow = Integer.parseInt(scan.nextLine()) - 1;
 
-            int shipRow = Integer.parseInt(position.substring(0, 1)) - 1;
-            scan.nextLine();
-
-            int shipCol = Integer.parseInt(position.substring(2, 3)) - 1;
-            scan.nextLine();
+            System.out.print("Where would you like to place ship " + (i + 1) + " (col): ");
+            int shipCol = Integer.parseInt(scan.nextLine()) - 1;
 
             System.out.print("What direction would you want it to go (horizontal/vertical): ");
             String direction = scan.nextLine();
 
             if (checkValid(shipRow, shipCol, direction, i + 1)) {
-                shipBoard[shipRow - 1][shipCol - 1] = new Ship("O");
+                addShips(shipRow, shipCol, direction, i + 1);
+            } else {
+                System.out.println("Ship can't be placed there");
+                i--;
             }
+
+            printShipBoard();
         }
     }
 
@@ -49,11 +73,11 @@ public class Player {
     private Boolean checkValid(int row, int col, String direction, int length) {
         for (int i = 0; i < length; i++) {
             if (direction.equals("horizontal")) {
-                if (!(shipBoard[row][col + i] instanceof Water)) {
+                if ((shipBoard[row][col + i] instanceof Ship)) {
                     return false;
                 }
             } else {
-                if (!(shipBoard[row + i][col] instanceof Water)) {
+                if ((shipBoard[row + i][col] instanceof Ship)) {
                     return false;
                 }
             }
