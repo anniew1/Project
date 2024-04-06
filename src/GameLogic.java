e84783aeae6d9e564e3966f2ddab93d614394bcb
Annie <anniew31@nycstudents.net>
2024-04-05 20:37:44 -0400

    made things look better and added some logic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -4,9 +4,11 @@ public class GameLogic {
     Scanner scan;
     Player p1;
     Player p2;
+    Boolean gameOver;
 
     public GameLogic() {
         scan = new Scanner(System.in);
+        gameOver = false;
     }
     public void startGame() {
         System.out.println("Basic Info: \n-A bomb hits 3 random spots in a row\n-A normal hit affects 1 spot\n-If you attack a spot more than once, nothing happens");
@@ -17,5 +19,20 @@ public class GameLogic {
         System.out.print("Player 2's Name: ");
         p2 = new Player(scan.nextLine());
 
+        while (!gameOver) {
+            takeTurn(p1, p2);
+            takeTurn(p2, p1);
+        }
+
     }
+
+    // prints out and does the actions for a player to attack
+    private void takeTurn(Player current, Player enemy) {
+        System.out.println(current.getName() + "'s Turn: ");
+        System.out.println("Enemy's Board: ");
+        enemy.printBoard(enemy.board);
+        current.attack(enemy);
+        enemy.printBoard(enemy.board);
+    }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -18,15 +18,36 @@ public class Player {
         bomb = new Bomb(3, "bomb");
     }
 
-    // prints out the board with the ships
-    public void printShipBoard() {
-        for (Water[] row : shipBoard) {
-            for (Water col : row) {
-                System.out.print(col.getSymbol());
+    // returns the name of the player
+    public String getName() {
+        return name;
+    }
+
+    // prints out the given board
+    public void printBoard(Water[][] board) {
+        System.out.println("   1  2  3  4  5  6  7  8  9  10");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j].getSymbol() + " ");
             }
             System.out.println();
         }
     }
+
+    // checks whether a person has eliminated all ships
+    public Boolean victory() {
+        for (Water[] row : shipBoard) {
+            for (Water col : row) {
+                if (col instanceof Ship) {
+                    return false;
+                }
+            }
+        }
+        System.out.println("You are the winner!!!");
+        return true;
+    }
+
 
     // places the ships
     private void addShips(int shipRow, int shipCol, String direction, int length) {
@@ -42,7 +63,7 @@ public class Player {
     //asks the player to place ships
     private void placeShip() {
         System.out.println("Ship # = how much spaces (Ship 1 = 1 space long, Ship 2 = 2 spaces long, etc.)");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1; i++) {
             System.out.print("Where would you like to place ship " + (i + 1) + " (row): ");
             int shipRow = Integer.parseInt(scan.nextLine()) - 1;
 
@@ -59,7 +80,7 @@ public class Player {
                 i--;
             }
 
-            printShipBoard();
+            printBoard(shipBoard);
         }
     }
 
@@ -67,8 +88,8 @@ public class Player {
     private void setupBoard() {
         for(int r = 0; r < board.length; r++) {
             for(int c = 0; c  < board[0].length; c++) {
-                board[r][c] = new Water("\uD83D\uDCA7");
-                shipBoard[r][c] = new Water("\uD83D\uDCA7");
+                board[r][c] = new Water("\uD83C\uDF0A");
+                shipBoard[r][c] = new Water("\uD83C\uDF0A");
             }
         }
     }
@@ -77,15 +98,18 @@ public class Player {
     private Boolean checkValid(int row, int col, String direction, int length) {
 
         for (int i = 0; i < length; i++) {
-            if (row > 10 || col > 10) {
-                return false;
-            }
 
             if (direction.equals("h")) {
+                if (col + length > 10) {
+                    return false;
+                }
                 if ((shipBoard[row][col + i] instanceof Ship)) {
                     return false;
                 }
             } else {
+                if (row + length > 10) {
+                    return false;
+                }
                 if ((shipBoard[row + i][col] instanceof Ship)) {
                     return false;
                 }
@@ -94,43 +118,44 @@ public class Player {
         return true;
     }
 
-    private void attack(){
-        Water shipFound = new Water("\uD83D\uDEA2");
+    public void attack(Player enemy){
+        Water shipFound = new Water("\uD83D\uDD25");
         Water nothingFound = new Water("❌");
         if(bomb.getInventory() > 0) {
             System.out.println("You have " + bomb.getInventory() + " bombs remaining");
-            System.out.print("Would you like to use a bomb? (y/n) ");
+            System.out.print("Would you like to use a bomb? (y/n): ");
             String choice = scan.nextLine();
             System.out.println();
+
             if(choice.equals("y")){
                 bomb.setInventory(bomb.getInventory() - 1);
                 System.out.print("Which row would you like to hit?");
-                int r = scan.nextInt();
+                int r = scan.nextInt() - 1;
                 System.out.print("Which column would you like to hit?");
-                int c = scan.nextInt();
+                int c = scan.nextInt() - 1;
                 //implementation which changes the board accordingly
                 if(c == 1){
                     for(int i = 0; i < c; i++){
                         if(shipBoard[r][i].getSymbol().equals("\uD83D\uDEA2")){
-                            board[r][i] = shipFound;
+                            enemy.board[r][i] = shipFound;
                         } else {
-                            board[r][i] = nothingFound;
+                            enemy.board[r][i] = nothingFound;
                         }
                     }
                 } else if (c == 10){
                     for(int j = 9; j >= c - 3; j--){
                         if(shipBoard[r][j].getSymbol().equals("\uD83D\uDEA2")){
-                            board[r][j] = shipFound;
+                            enemy.board[r][j] = shipFound;
                         } else {
-                            board[r][j] = nothingFound;
+                            enemy.board[r][j] = nothingFound;
                         }
                     }
                 } else {
                     for(int k = c - 1; k <= c + 1; k++){
                         if(shipBoard[r][k].getSymbol().equals("\uD83D\uDEA2")){
-                            board[r][k] = shipFound;
+                            enemy.board[r][k] = shipFound;
                         } else {
-                            board[r][k] = nothingFound;
+                            enemy.board[r][k] = nothingFound;
                         }
                     }
                 }
@@ -140,9 +165,9 @@ public class Player {
                 System.out.print("Which column would you like to hit?");
                 int c = scan.nextInt();
                 if(shipBoard[r][c].getSymbol().equals("\uD83D\uDEA2")){
-                    board[r][c] = shipFound;
+                    enemy.board[r][c] = shipFound;
                 } else {
-                    board[r][c] = nothingFound;
+                    enemy.board[r][c] = nothingFound;
                 }
             }
         } else {
@@ -151,11 +176,10 @@ public class Player {
             System.out.print("Which column would you like to hit?");
             int c = scan.nextInt();
             if(shipBoard[r][c].getSymbol().equals("\uD83D\uDEA2")){
-                board[r][c] = shipFound;
+                enemy.board[r][c] = shipFound;
             } else {
-                board[r][c] = nothingFound;
+                enemy.board[r][c] = nothingFound;
             }
-
         }
     }
 
